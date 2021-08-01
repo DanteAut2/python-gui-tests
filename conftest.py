1dfd3869c512aaab504a590c897363b47d12e210
DanteAut2 <79665248+DanteAut2@users.noreply.github.com>
2021-08-01 18:38:57 +0300

    задание 24

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
+import os
 import pytest
-
 from fixture.application import Application
+from comtypes.client import CreateObject
 
 
 @pytest.fixture(scope="session")
@@ -8,3 +9,22 @@ def app(request):
     fixture = Application("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
     return fixture
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("xlsx_"):
+            testdata = load_from_excel(fixture[5:])
+            metafunc.parametrize(fixture, testdata)
+
+
+def load_from_excel(file):
+    xlsx_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"fixture/{file}.xlsx")
+    app = CreateObject("Excel.Application")
+    wb = app.Workbooks.Open(xlsx_file)
+    worksheet = wb.Sheets[1]
+    test_data = []
+    for row in range(1, 100):
+        data = worksheet.Cells[row, 1].Value()
+        if data is not None:
+            test_data.append(data)
+    return test_data
